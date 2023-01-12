112031d49d3d3a0f5f224b867394ddcf0875d21f
maormarhum <maormarhum9400@gmail.com>
2023-01-13 00:38:01 +0200

    final

--- Cocktails/src/pages/About.js
+++ Cocktails/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus saepe eos ullam molestias soluta vitae eum ipsum eaque necessitatibus, quasi, nemo ratione voluptatibus quis, ipsa corrupti consequatur excepturi blanditiis tempora quibusdam optio officia facere aut consequuntur. Laudantium ipsa quod consectetur.</p>
+    </section>
   )
 }
 
